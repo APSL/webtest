bf6ccd2cab8af2531a6fd3a450b90d11c7ea14d5
tolo <tolo@pc-tolo.(none)>
2015-04-22 15:01:44 +0200

    Añadido tema de estadisticas con influx DB

--- webtest/__init__.py
+++ webtest/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # author: bcabezas@apsl.net
 
-__VERSION__ = '0.0.3'
+__VERSION__ = '0.0.4'
 from .base import WebTest, step
 from .loader import get_test
 from .multimech import BaseTransaction
--- webtest/base.py
+++ webtest/base.py
@@ -7,6 +7,7 @@ import inspect
 import logging
 import traceback
 import sys
+from influxdb.influxdb08 import InfluxDBClient
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -91,13 +92,20 @@ class WebTest(object):
             self.driver = dict(self.DRIVER_CHOICES)[driver](**kwargs)
         except KeyError:
             self.driver = webdriver.PhantomJS()
+        self.timeout = timeout
         self.driver.implicitly_wait(timeout)
         self.wait = WebDriverWait(self.driver, timeout)
         self.url = url or self.URL
 
-    def wait_for_id(self, name):
+    def close(self):
+        self.driver.quit()
+
+    def wait_for_id(self, name, timeout=None):
         """calls selenium webdriver wait for ID name"""
-        self.wait.until(EC.presence_of_element_located((By.ID, name)))
+        if not timeout:
+            timeout = self.timeout
+        WebDriverWait(self.driver, timeout).until(
+                EC.presence_of_element_located((By.ID, name)))
 
     def wait_for_class(self, name):
         """calls selenium webdriver wait for class name"""
@@ -106,6 +114,10 @@ class WebTest(object):
     def wait_for_xpath(self, name):
         """calls selenium webdriver wait for xpath"""
         self.wait.until(EC.visibility_of_element_located((By.XPATH, name)))
+
+    def wait_for_css_selector(self, name):
+        """calls selenium webdriver wait for css"""
+        self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, name)))
 
     def wait_for_css_selector_in_element(self, web_element, name):
         """calls selenium webdriver wait for css, search in web_element"""
@@ -119,19 +131,46 @@ class WebTest(object):
         for s in steps:
             yield s()
 
-    def run(self):
+    def run(self, stats=False, stats_name='webtest'):
+        ok_stats = []
+        err_stat = None
+
+        init_test_time = time.time()
         for elapsed, name, doc, error in self:
             if error:
                 print u"ERROR {name} in {elapsed:10.2f}s ({doc}) --> [[{error}]]".format(**locals())
                 #self.driver.save_screenshot('error-{}.png'.format(name))
-                sys.exit(1)
+                err_stat = [time.time(), name, error]
+                break
             else:
                 print u"Run {name} in {elapsed:10.2f}s ({doc})".format(**locals())
                 #self.driver.save_screenshot('ok-{}.png'.format(name))
+                ok_stats.append([time.time(), name, elapsed])
+        elapsed_test_time = time.time() - init_test_time
+        print u"Total in {}".format(elapsed_test_time)
+
+        if stats:
+            client = InfluxDBClient("localhost", 8086, "root", "root", "mydata")
+            points =    [{
+                            'points': [[time.time(), "total", elapsed_test_time]],
+                            'name': stats_name,
+                            'columns': ['time', "step", "elapsed"]
+                        }]
+            if err_stat:
+                points.append({
+                            'points': [err_stat],
+                            'name': '{}_errors'.format(stats_name),
+                            'columns': ['time', "step", "error"]
+                        })
+            if ok_stats:
+                points.append({
+                        'points': ok_stats,
+                        'name': stats_name,
+                        'columns': ['time', 'step', 'elapsed']
+                    })
+            client.write_points(points)
         self.close()
 
-    def close(self):
-        self.driver.quit()
 
 
 if __name__ == "__main__":
